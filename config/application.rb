3dea91ea2eed8ea080d3b4c4d9208e9a4373209c
duonghienan <andh@asiantech.vn>
2015-09-18 15:18:57 +0700

    Add config use Session for rails api

--- config/application.rb
+++ config/application.rb
@@ -25,9 +25,9 @@ module DemoApiForIms
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    # config.middleware.use Rack::MethodOverride
-    # config.middleware.use ActionDispatch::Cookies
+    config.middleware.use Rack::MethodOverride
+    config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
-    # config.middleware.use ActionDispatch::Flash
+    config.middleware.use ActionDispatch::Flash
   end
 end
