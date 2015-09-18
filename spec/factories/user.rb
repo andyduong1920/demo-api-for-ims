e517796a90ad9a1e6bf2c6ac569c5fcd6afc3177
Cao Quang Binh <binhcq@asiantech.vn>
2015-09-18 17:24:17 +0700

    [Update] Use Faker with FactoryGirl

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
 	factory :user do
-		sequence(:email) { |n| "BinhCQ#{n}@asiantech.vn"}
-		password "12345678"
+    email { Faker::Internet.email }
+		password { Faker::Internet.password(8) }
 	end	
 end
