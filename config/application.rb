e4ae91f4e4bc4b59945d385c3788ff004ef58678
Cao Quang Binh <binhcq@asiantech.vn>
2015-09-18 14:25:50 +0700

    [Add] init and config rspec

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,16 @@ module DemoApiForIms
     # config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
     # config.middleware.use ActionDispatch::Flash
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
   end
 end
